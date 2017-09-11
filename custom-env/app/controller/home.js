e949c5a2d20e4912a1e5edfa7035e9811fba34f3
haichuan_liang <haichuan_liang@crfchina.com>
2017-09-11 11:19:28 +0800

    Initial commit

---
+++ custom-env/app/controller/home.js
@@ -0,0 +1,9 @@
+'use strict';
+
+exports.index = function* index(ctx) {
+  ctx.body = {
+    env: ctx.app.config.env,
+    config: ctx.app.config.keys,
+    url: ctx.app.config.url,
+  };
+};
---
+++ custom-env/config/config.default.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = {
+  keys: 'default keys',
+  url : 'ccc',
+};
